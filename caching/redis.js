06cd4255e5fe8b9c959745adb149d3148a93fc8a
Soumendu Roy <roysoumendu765@gmail.com>
2025-02-02 15:52:20 +0530

    fix: updated caching config

--- caching/redis.js
+++ caching/redis.js
@@ -2,7 +2,7 @@ const redis = require("redis");
 require('dotenv').config();
 
 const client = redis.createClient({
-    url: process.env.REDIS_URI || "redis://127.0.0.1:6381"
+    url: process.env.REDIS_URI || "redis://127.0.0.1:6379"
 });
 
 client.on("error", (err) => console.error("Redis Error:", err));
