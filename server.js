c7870087f72ee4e5d59cd1ee006bc2033cf458f5
Soumendu Roy <roysoumendu765@gmail.com>
2025-02-02 15:58:42 +0530

    fix: updated server logic

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const routes = require("./routes/routes");
 const connect = require('./connection/connection');
@@ -11,6 +12,8 @@ app.use(express.json());
 
 connect();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -20,8 +23,15 @@ app.use((req, res, next) => {
 
 
 app.use('/api',routes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is Up and Running at ${PORT}`);
 })
+
+module.exports = app;
